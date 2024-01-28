82b9d569daa9f6e264733a07dc9176901cd6a495
zyk326 <869550165@qq.com>
2024-01-28 17:59:27 +0800

    finish 3-3

--- Learning/code-tree/LanQiao_rules/algorithm_foundation/test.py
+++ Learning/code-tree/LanQiao_rules/algorithm_foundation/test.py
@@ -1,56 +1,36 @@
-N = 100001
-h = [-1 for i in range(N)]
-w = [0 for i in range(N)]
-e = [0 for i in range(N)]
-ne = [0 for i in range(N)]
-idx = 0
-dist = [N for i in range(N)]
-st = [0 for i in range(N)]
-
-def add(a, b, c):
-    global idx
-    e[idx] = b
-    w[idx] = c
-    ne[idx] = h[a]
-    h[a] = idx
-    idx += 1
-
-def spfa():
-    dist[1] = 0
-    q = []
-    q.append(1)
-    st[1] = 1
-    while(len(q)):
-        t = q[0]
-        del q[0]
-        st[t] = 0
-        i = h[t]
-        while(i != -1):
-            j = e[i]
-            if(dist[j] > dist[t] + w[i]):
-                dist[j] = dist[t] + w[i]
-                if(st[j] == 0):
-                    q.append(j)
-                    st[j] = 1
-            i = ne[i]
-    if(dist[n] == N):
-        return -1
-    else:
-        return dist[n]
+def find(x):
+    if (p[x] != x):
+        p[x] = find(p[x])
+    return p[x]
 
 if __name__ == '__main__':
     n, m = input().split()
     n = int(n)
-    m = int(m)
-
-    while(m):
+    m = int(m) 
+    mm = m
+    N = 100001
+    edge = []
+    p = [0 for i in range(N)]
+    while(mm):
         a, b, c = input().split()
         a = int(a)
         b = int(b)
         c = int(c)
-        add(a, b, c)
-        m -= 1
-    
-    t = spfa()
-
-    print(t)
+        edge.append([a, b, c])
+        mm -= 1
+    edge = sorted(edge, key = lambda edge:edge[2])
+    for i in range(1, n + 1):
+        p[i] = i
+    res = 0
+    cnt = 0
+    for i in range(m):
+        aa, bb, cc = edge[i][0],edge[i][1],edge[i][2]
+        aa, bb = find(aa), find(bb)
+        if aa != bb:
+            p[aa] = bb
+            res += cc
+            cnt += 1
+    if cnt < n - 1:
+        print("impossible")
+    else:
+        print(res)
