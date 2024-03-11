9b98fd723acdaf3b63da9d1a37c5296b972e0dcf
zyk326 <869550165@qq.com>
2024-03-11 22:29:53 +0800

    finish 3-11

--- Learning/code-tree/LanQiao_rules/Daily Practice/test.py
+++ Learning/code-tree/LanQiao_rules/Daily Practice/test.py
@@ -1,33 +1,35 @@
-def get(i, ti):
-    return max(0, fishes[i] - ti * loss[i])
+# 3498 日期差值
+def is_leap(y):
+    if (y % 4 == 0 and y % 100 != 0) or y % 400 == 0:
+        return 1
+    return 0
 
-def work(i, t):
-    ans = 0
-    ti = [0 for i in range(N + 1)]
+def get_days(y, i):
+    if i == 2:
+        return 28 + is_leap(y)
+    return months[i]
 
-    for s in range(1, t + 1):
-        j = 1
-        for k in range(2, i + 1):
-            if get(k, ti[k]) > get(j, ti[j]):
-                j = k
-        ans += get(j, ti[j])
-        ti[j] += 1
-    return ans
+def calc(y, m, d):
+    y = int(y)
+    m = int(m)
+    d = int(d)
+    days = 0
+    for i in range(1, y):
+        days += 365 + is_leap(i)
+    for i in range(1, m):
+        days += get_days(y, i)
+    return days + d
 
 if __name__ == '__main__':
-    N = int(input())
-    fishes = list(map(int, input().strip().split()))
-    loss = list(map(int, input().strip().split()))
-    totime = list(map(int, input().strip().split()))
-    fishes.insert(0, 0)
-    loss.insert(0, 0)
-    totime.insert(0, 0)
-    totime.insert(0, 0)
-    for i in range(2, N + 1):
-        totime[i] += totime[i - 1]
-    T = int(input())
-    d = [0 for i in range(1, N + 1)]
-    res = 0
-    for i in range(1, N + 1):
-        res = max(res, work(i, T - totime[i]))
-    print(res)
+    months = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    ans = []
+    while True:
+        try:
+            st = input()
+            ed = input()
+            res = abs(calc(st[0:4], st[4:6], st[6:8]) - calc(ed[0:4], ed[4:6], ed[6:8])) + 1 
+            ans.append(res)
+        except:
+            break
+    for i in ans:
+        print(i)
