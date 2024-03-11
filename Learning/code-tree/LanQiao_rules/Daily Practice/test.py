890c06cc949c77ca97e07206e80ad87cf4c283e7
zyk326 <869550165@qq.com>
2024-03-11 13:08:15 +0800

    finish 3-7

--- Learning/code-tree/LanQiao_rules/Daily Practice/test.py
+++ Learning/code-tree/LanQiao_rules/Daily Practice/test.py
@@ -1,55 +1,33 @@
-def work(a):
-    buf = [[]]
-    for i in range(1, n + 1):
-        bufs = [a[i]]
-        bufs.append(i)
-        buf.append(bufs)
-    buf[1:] = sorted(buf[1:], key = lambda k:k[0])
-    for i in range(1, n + 1):
-        a[buf[i][1]] = i
+def get(i, ti):
+    return max(0, fishes[i] - ti * loss[i])
 
-def merge_res(l, r):
-    if l >= r:
-        return 0
-    mid = (l + r) >> 1
-    res = (merge_res(l, mid) + merge_res(mid + 1, r)) % mod
-    i, j, k = l, mid + 1, 0
-    while(i <= mid and j <= r):
-        if b[i] < b[j]:
-            p[k] = b[i]
-            k += 1
-            i += 1
-        else:
-            p[k] = b[j]
-            k += 1
-            j += 1
-            res = (res + mid - i + 1) % mod
-    while(i <= mid):
-        p[k] = b[i]
-        k += 1
-        i += 1
-    while(j <= r):
-        p[k] = b[j]
-        k += 1
-        j += 1
-    b[l:l + k] = p[:k]
-    return res
+def work(i, t):
+    ans = 0
+    ti = [0 for i in range(N + 1)]
+
+    for s in range(1, t + 1):
+        j = 1
+        for k in range(2, i + 1):
+            if get(k, ti[k]) > get(j, ti[j]):
+                j = k
+        ans += get(j, ti[j])
+        ti[j] += 1
+    return ans
 
 if __name__ == '__main__':
-    n = int(input())
-    a = list(map(int, input().strip().split()))
-    b = list(map(int, input().strip().split()))
-    a.insert(0, 0)
-    b.insert(0, 0)
-    N = 100010
-    mod = 99999997
-    c = [0 for i in range(N)]
-    p = [0 for i in range(N)]
-    work(a)
-    work(b)
-    for i in range(1, n + 1):
-        c[a[i]] = i
-    for i in range(1, n + 1):
-        b[i] = c[b[i]]
-    res = merge_res(1, n)
+    N = int(input())
+    fishes = list(map(int, input().strip().split()))
+    loss = list(map(int, input().strip().split()))
+    totime = list(map(int, input().strip().split()))
+    fishes.insert(0, 0)
+    loss.insert(0, 0)
+    totime.insert(0, 0)
+    totime.insert(0, 0)
+    for i in range(2, N + 1):
+        totime[i] += totime[i - 1]
+    T = int(input())
+    d = [0 for i in range(1, N + 1)]
+    res = 0
+    for i in range(1, N + 1):
+        res = max(res, work(i, T - totime[i]))
     print(res)
