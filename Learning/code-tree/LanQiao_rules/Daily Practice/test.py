be50210d76184815b8c799c314a75a598017f77a
zyk326 <869550165@qq.com>
2024-03-12 21:05:29 +0800

    finish 3-12

--- Learning/code-tree/LanQiao_rules/Daily Practice/test.py
+++ Learning/code-tree/LanQiao_rules/Daily Practice/test.py
@@ -1,35 +1,23 @@
-# 3498 日期差值
-def is_leap(y):
-    if (y % 4 == 0 and y % 100 != 0) or y % 400 == 0:
-        return 1
-    return 0
-
-def get_days(y, i):
-    if i == 2:
-        return 28 + is_leap(y)
-    return months[i]
-
-def calc(y, m, d):
-    y = int(y)
-    m = int(m)
-    d = int(d)
-    days = 0
-    for i in range(1, y):
-        days += 365 + is_leap(i)
-    for i in range(1, m):
-        days += get_days(y, i)
-    return days + d
-
+# 1343 挤牛奶
+# 区间合并
+# 思路维护一个连续区间的左端点和右端点
+# 按左端点排序,查看一个新区间的时候,更新连续区间的左, 右端点
 if __name__ == '__main__':
-    months = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    ans = []
-    while True:
-        try:
-            st = input()
-            ed = input()
-            res = abs(calc(st[0:4], st[4:6], st[6:8]) - calc(ed[0:4], ed[4:6], ed[6:8])) + 1 
-            ans.append(res)
-        except:
-            break
-    for i in ans:
-        print(i)
+    N = int(input())
+    inf = [[0, 1e7]]
+    for i in range(N):
+        inf.append(list(map(int, input().split())))
+    inf[1:] = sorted(inf[1:], key=lambda k:k[0]) # 区间排序,区间覆盖
+    l = 1
+    res1, res2 = 0, 0
+    mr = inf[1][1] # 最右端点
+    for i in range(1, N + 1):
+        if inf[i][0] <= mr and inf[i][1] >= mr: # 加等于是为了算第一个区间值, 否则需要初始化
+            res1 = max(res1, inf[i][1] - inf[l][0])
+            mr = max(mr, inf[i][1])
+        if inf[i][0] > mr: # 算距离,第一个元素的右端点不能在第一个区间的左端点右边,所以初始化的时候需要把填充端点的右端点无限往右拉
+            res2 = max(res2, inf[i][0] - mr)
+            l = i
+            mr = inf[i][1]
+    res1 = max(res1, inf[N][1] - inf[N][0]) # 最后一个区间初始化
+    print("%d %d"%(res1, res2))
